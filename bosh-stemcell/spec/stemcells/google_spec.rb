a7cfb405e4ce9d63d1b19adf9ffea493d3ccadec
Evan Brown <evanbrown@google.com>
2016-09-21 20:11:25 -0700

    Update system guest package for Google stemcell
    This change uses the latest `compute-image-packages` for Google guest VMs.
    The new package allows more granular configuration of daemons and improves
    compatibility with other services on boot.
    
    https://github.com/GoogleCloudPlatform/compute-image-packages/releases/tag/20160803

--- bosh-stemcell/spec/stemcells/google_spec.rb
+++ bosh-stemcell/spec/stemcells/google_spec.rb
@@ -24,13 +24,25 @@ describe 'Google Stemcell', stemcell_image: true do
     case ENV['OS_NAME']
       when 'ubuntu'
         [
-          '/etc/init/google-accounts-manager-service.conf',
-          '/etc/init/google-accounts-manager-task.conf',
-          '/etc/init/google-clock-sync-manager.conf'
+          '/var/lib/dpkg/info/google-compute-engine-init-wheezy.postinst',
+          '/var/lib/dpkg/info/google-compute-engine-init-wheezy.conffiles',
+          '/var/lib/dpkg/info/google-compute-engine-wheezy.md5sums',
+          '/var/lib/dpkg/info/google-compute-engine-init-wheezy.md5sums',
+          '/var/lib/dpkg/info/google-compute-engine-wheezy.list',
+          '/var/lib/dpkg/info/google-compute-engine-init-wheezy.prerm',
+          '/var/lib/dpkg/info/google-compute-engine-init-wheezy.list',
+          '/etc/init.d/google-accounts-daemon',
+          '/etc/init.d/google-instance-setup',
+          '/etc/init.d/google-clock-skew-daemon',
+          '/etc/init.d/google-ip-forwarding-daemon',
+          '/usr/bin/google_instance_setup',
+          '/usr/bin/google_ip_forwarding_daemon',
+          '/usr/bin/google_accounts_daemon',
+          '/usr/bin/google_clock_skew_daemon',
+          '/usr/bin/google_metadata_script_runner',
         ].each do |conf_file|
           describe file(conf_file) do
             it { should be_file }
-            it { should be_mode(mode) }
             it { should be_owned_by(owner) }
             it { should be_grouped_into(group) }
           end
