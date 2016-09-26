fedf12c0a17e8da5902998d89c0f28f72d7ff039
Brian Cunnie <bcunnie@pivotal.io>
2016-09-26 11:45:57 -0700

    Google Ubuntu Stemcells use Ubuntu-specific packages
    - updated packages are ubuntu-specific, use upstart instead of System V init,
      cooperate with BOSH's customized rsyslog start behavior
    - revert rsyslog to original behavior: waits until /var/log is mounted
    - removed `rsyslog_config` stage, was performed earlier in os_image
      stages for both Ubuntu and CentOS.
    
    fixes:
    ```
    The following packages have unmet dependencies:
     google-compute-engine-init-ubuntu-trusty : Depends: google-compute-engine-ubuntu-trusty but it is not installable
                                                Depends: google-config-ubuntu-trusty but it is not installable
     google-compute-engine-wheezy : Depends: google-compute-engine-init-wheezy but it is not installable
    E: Unmet dependencies. Try 'apt-get -f install' with no packages (or specify a solution).
    ```
    
    [#130561911](https://www.pivotaltracker.com/story/show/130561911)

--- stemcell_builder/stages/system_google_packages/config.sh
+++ stemcell_builder/stages/system_google_packages/config.sh
@@ -9,8 +9,11 @@ source $base_dir/lib/prelude_config.bash
 cd $assets_dir
 
 # Download google-image-packages
-wget https://storage.googleapis.com/bosh-stemcell-artifacts/google-compute-engine-init-ubuntu-trusty_2.1.0-0.1474671297_amd64.deb
-echo "8081f1a3a92c7a64b762f8382dc42c952633cb08  google-compute-engine-init-ubuntu-trusty_2.1.0-0.1474671297_amd64.deb" | sha1sum -c -
+wget https://storage.googleapis.com/bosh-cpi-artifacts/google-compute-engine-init-trusty_2.1.0-0.1474913068_amd64.deb
+echo "229a1cd551b865cab199516e7572412fa4fde903  google-compute-engine-init-trusty_2.1.0-0.1474913068_amd64.deb" | sha1sum -c -
 
-wget https://storage.googleapis.com/bosh-stemcell-artifacts/google-compute-engine-wheezy_2.1.3-0.1474395669_all.deb
-echo "820d7d06afd7a60d884fea1570a6e5ba108b967b  google-compute-engine-wheezy_2.1.3-0.1474395669_all.deb" | sha1sum -c -
+wget https://storage.googleapis.com/bosh-stemcell-artifacts/google-compute-engine-trusty_2.2.3-0.1474912841_all.deb
+echo "0da71ccd637145f34ef4e97bcdc741d5b8177081  google-compute-engine-trusty_2.2.3-0.1474912841_all.deb" | sha1sum -c -
+
+wget https://storage.googleapis.com/bosh-stemcell-artifacts/google-config-trusty_2.0.0-0.1474912881_amd64.deb
+echo "d8cc6556a73e5766a032230b900f6a24e30e66df  google-config-trusty_2.0.0-0.1474912881_amd64.deb" | sha1sum -c -
